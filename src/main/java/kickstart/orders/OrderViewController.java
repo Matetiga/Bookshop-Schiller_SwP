7684d40deb28025d318be9f85d33f6bd03cddb05
vinc <vincent.richter14@gmail.com>
2024-11-22 20:30:57 +0100

    update Order Overview

--- src/main/java/kickstart/orders/OrderViewController.java
+++ src/main/java/kickstart/orders/OrderViewController.java
@@ -2,10 +2,14 @@ package kickstart.orders;
 
 import org.salespointframework.order.Order;
 import org.salespointframework.order.OrderStatus;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 
 @Controller
 public class OrderViewController {
@@ -24,9 +28,32 @@ public class OrderViewController {
 		return "order-overview";
 	}
 
+	@PostMapping("/showProducts")
+	String showProducts(@RequestParam("orderId") Order.OrderIdentifier orderId, Model model){
+		//model.addAttribute("show", true);
+		return "order-overview";
+	}
+
 	@PostMapping("/deleteOrder")
 	String deleteOrder(@RequestParam("orderId") Order.OrderIdentifier orderId, Model model){
 		myOrderRepository.deleteById(orderId);
+		model.addAttribute("orderList", myOrderRepository.findAll());
+		return "order-overview";
+	}
+
+	@PostMapping("/changeStatus")
+	String changeStatus(@RequestParam("orderId") Order.OrderIdentifier orderId, Model model){
+		MyOrder order = myOrderRepository.findById(orderId).get();
+		order.changeStatus();
+		myOrderRepository.save(order);
+		model.addAttribute("orderList", myOrderRepository.findAll());
+		return "order-overview";
+	}
+
+	@PostMapping("/sortByDate")
+	String sortByDate(Model model){
+		//ArrayList<MyOrder> orderList = myOrderRepository.findAll().iterator();
+		//orderList.sort(Comparator);
 		model.addAttribute("orderList", myOrderRepository.findAll());
 		return "order-overview";
 	}
@@ -44,12 +71,4 @@ public class OrderViewController {
 		return "order-overview";
 	}
 
-	@PostMapping("/changeStatus")
-	String changeStatus(@RequestParam("orderId") Order.OrderIdentifier orderId, Model model){
-		MyOrder order = myOrderRepository.findById(orderId).get();
-		order.changeStatus();
-		myOrderRepository.save(order);
-		model.addAttribute("orderList", myOrderRepository.findAll());
-		return "order-overview";
-	}
 }
