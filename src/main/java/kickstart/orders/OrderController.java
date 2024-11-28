f7f50167b24673a90b28266ca92a9843bf4255c3
vinc <vincent.richter14@gmail.com>
2024-11-28 17:17:08 +0100

    "Not enough items in stock"-message added in cart

--- src/main/java/kickstart/orders/OrderController.java
+++ src/main/java/kickstart/orders/OrderController.java
@@ -91,10 +91,11 @@ public class OrderController {
 				deleteProductsFromStock(order);
 
 			}catch(IllegalArgumentException e){
-				model.addAttribute("error_NotEnoughStock", e.getMessage());
+				model.addAttribute("error_NotEnoughStock", true);
 				return "cart";
 			}
 
+			//model.addAttribute("error_NotEnoughStock", false);
 			myOrderRepository.save(order);
 			cart.clear();
 		}
@@ -118,9 +119,10 @@ public class OrderController {
 			inventory.findByProductIdentifier(orderLine.getProductIdentifier()).ifPresent(item -> {
 				if(item.getQuantity().isLessThan(orderLine.getQuantity())){
 					throw new IllegalArgumentException("Not enough items in stock");
+				}else{
+					item.decreaseQuantity(orderLine.getQuantity());
+					inventory.save(item);
 				}
-				item.decreaseQuantity(orderLine.getQuantity());
-				inventory.save(item);
 			});
 		}
 	}
