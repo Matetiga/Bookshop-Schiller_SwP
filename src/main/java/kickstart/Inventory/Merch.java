bd70fb28b7bd0fd1e64729d0507bdded2210e177
Marco <marcoihdl@gmail.com>
2025-01-16 20:03:55 +0100

    Finish javadocs for inventory

--- src/main/java/kickstart/Inventory/Merch.java
+++ src/main/java/kickstart/Inventory/Merch.java
@@ -5,11 +5,20 @@ import org.javamoney.moneta.Money;
 
 @Entity
 public class Merch extends ShopProduct {
-
+	/**
+	 * Basic constructor
+	 * @param name
+	 * @param image
+	 * @param price
+	 * @param description
+	 */
 	public Merch(String name, String image, Money price, String description) {
 		super(name, image, price, description);
 	}
 
+	/**
+	 * Empty constructor
+	 */
 	public Merch() {
 
 	}
